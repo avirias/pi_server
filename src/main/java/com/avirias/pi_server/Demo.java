53503433027753edef17cbe936cac256ce37d6a7
avirias <avirias@live.com>
2020-03-12 13:06:59 +0530

    Second Commit

--- src/main/java/com/avirias/pi_server/Demo.java
+++ src/main/java/com/avirias/pi_server/Demo.java
@@ -1,7 +1,16 @@
 package com.avirias.pi_server;
 
+import com.avirias.pi_server.dto.ConnectDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * User: avirias
@@ -12,7 +21,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class Demo {
 
     @GetMapping
-    public String getHello(){
-        return  "Hello World";
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/connect")
+    public ResponseEntity connect(@RequestBody ConnectDto connectDto) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "curl -w \"\\n\" -d '{\"ssid\":\"" + connectDto.getSsid() + "\", \"psk\":\""
+                + connectDto.getPassword() + "\"}' \\\n" +
+                "     -H \"Content-Type: application/json\" \\\n" +
+                "     -X POST localhost:8080/connect");
+
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body("Connected");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
     }
 }
---
+++ src/main/java/com/avirias/pi_server/dto/ConnectDto.java
@@ -0,0 +1,14 @@
+package com.avirias.pi_server.dto;
+
+import lombok.Getter;
+
+/**
+ * User: avirias
+ * Date: 12/03/20 1:00 PM
+ */
+
+@Getter
+public class ConnectDto {
+    private String ssid;
+    private String password;
+}
